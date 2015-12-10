2b86ae555953eb2c2d2bbad7b1f3bbb403238201
alekum <rojaster@yandex.ru>
2015-12-10 22:31:50 +0300

    Deleted compareTo test method

--- src/test/java/org/astec/jdotlib/dotgraph/attributes/AttributeTest.java
+++ src/test/java/org/astec/jdotlib/dotgraph/attributes/AttributeTest.java
@@ -1,11 +1,9 @@
 package org.astec.jdotlib.dotgraph.attributes;
 
-import org.astec.jdotlib.dotgraph.attributes.types.Colors;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static com.google.common.truth.Truth.assert_;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -59,24 +57,4 @@ public class AttributeTest
         assertEquals("color=\"black\";", attr.toString());
     }
 
-    /**
-     * Method: compareTo(Attribute o)
-     */
-    @Test
-    public void testCompareTo() throws Exception
-    {
-        Attribute attr0 = new Attribute("color", Colors.AQUA);
-        Attribute attr1 = new Attribute("color", Colors.CYAN);
-        Attribute attr2 = new Attribute("color", Colors.AQUA);
-
-        assert_().that(attr0).isEquivalentAccordingToCompareTo(attr2);
-        assert_().that(attr1).isNotSameAs(attr2);
-        assert_().that(attr0).isSameAs(attr0);
-        assert_().that(attr2).isNotEqualTo(attr1);
-
-        assert_().that(attr0.compareTo(attr2)).isEqualTo(0);
-        assert_().that(attr0.compareTo(attr1)).isNotEqualTo(0);
-        assert_().that(attr1.compareTo(attr0)).isNotEqualTo(0);
-        assert_().that(attr0.compareTo(attr0)).isEqualTo(0);
-    }
 } 
